667402d8c5411212614314d94fd8029a226c4e72
Miller Alvarez <mdalvarezb@ufpso.edu.co>
2024-07-17 21:23:37 -0500

    Decimo commit

--- config/configPg.js
+++ config/configPg.js
@@ -1,13 +1,16 @@
 const promise = require('bluebird');
 const pgp = require('pg-promise')({ promiseLib: promise });
-require('dotenv').config(); // Cargar las variables de entorno
+require('dotenv').config();
 
 const databaseConfig = {
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
     user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD
+    password: process.env.PG_PASSWORD,
+    ssl: {
+        rejectUnauthorized: false 
+    }
 };
 
 const db = pgp(databaseConfig);
