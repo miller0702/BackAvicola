1ec2ff523394756507279aa1fad12dd62d3b9395
Miller Alvarez <mdalvarezb@ufpso.edu.co>
2024-07-17 21:03:40 -0500

    Quinto commit

--- config/config.js
+++ config/config.js
@@ -1,23 +1,20 @@
-
-const mongoose = require('mongoose')
-
-//Conexión Postgres
-
+const mongoose = require('mongoose');
 
 const mongoBd = async () => {
-
     try {
-
         const connection = await mongoose.connect('mongodb+srv://admin:admin@cluster0.pqb3kvy.mongodb.net/app-avicola?retryWrites=true&w=majority', {
-        })
-        const url = `${connection.connection.host}:${connection.connection.port}`
-        console.log(`MongoDB conectado en: ${url}`)
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: true,
+            useCreateIndex: true
+        });
+
+        const url = `${connection.connection.host}:${connection.connection.port}`;
+        console.log(`MongoDB conectado en: ${url}`);
     } catch (error) {
-        console.log(`error: ${error.message}`);
+        console.error(`Error al conectar con MongoDB: ${error.message}`);
         process.exit(1);
     }
-}
+};
 
-module.exports = mongoBd
-
-
+module.exports = mongoBd;
