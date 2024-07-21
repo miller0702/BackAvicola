4dd62cb12be2256a0fadf1ce6c3b13963c049c49
Miller Alvarez <mdalvarezb@ufpso.edu.co>
2024-07-21 15:59:36 -0500

    Actualizacion grande

--- config/configFirebase.js
+++ config/configFirebase.js
@@ -1,9 +1,7 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./service.json');
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
     storageBucket: "appavicola-fc91e.appspot.com",
   });
 }
